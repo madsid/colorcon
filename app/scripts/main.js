7d8bf7f8fdab7b240ec6f19c28a6a9864f537a31
sidhartha43 <sidhartha43@gmail.com>
2015-06-27 01:37:31 -0500

    Latest

---
+++ app/scripts/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    /* Setting the box height to the window size on browser*/
+    $('.box').height($(window).height() - 115 + "px");
+
+    /* All events that must happen if window resizes */
+    $(window).resize(function () {
+       // $('.box').height($(document).height() - 115 + "px"); //Issue background image Disaapears
+    });
+
+    /* code for color box */
+    $('.color').click(function () {
+        console.log($(this)[0].id);
+    });
+
+    /* Display on scroll animation */
+    $(document).scroll(function () {
+        //Show element after user scrolls 800px
+        var y = $(this).scrollTop();
+        if (y > 800) {
+            //$('.bottomMenu').fadeIn();
+        } else {
+            //$('.bottomMenu').fadeOut();
+        }
+
+        // Show element after user scrolls past 
+        // the top edge of its parent 
+        $('.anim').each(function () {
+            var t = $(this).parent().offset().top - 60;
+            if (y > t) {
+                $(this).fadeIn();
+            } else {
+                $(this).fadeOut();
+            }
+        });
+    });
+
+    /* smooth Scrool code */
+
+    /* fire event on the keyboard scroll */
+    $(window).keydown(function (e) { // A Key has been Pressed
+        if (e.target.tagName.toLowerCase() !== 'input' && e.target.tagName.toLowerCase() !== 'textarea') { // If it's not in a input or textarea
+            switch (e.which) { // Figure out which one it is
+                case 37:
+                    // Left Arrow
+                    handle(1); // Scroll Up
+                    break; // Stop
+                case 38:
+                    // Up Arrow
+                    handle(1); // Scroll Up
+                    break; // Stop
+                case 39:
+                    // Right Arrow
+                    handle(-1); // Scroll Down
+                    break; // Stop
+                case 40:
+                    // Down Arrow
+                    handle(-1); // Scroll Down
+                    break; // Stop
+                default:
+                    return; // If it wasn't any of those, carry on.
+            }
+            if (event.preventDefault) event.preventDefault(); // Stop it if we can.
+        }
+    });
+
+    /* fire event on mouse scroll */
+    if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+    window.onmousewheel = document.onmousewheel = wheel;
+
+    function wheel(event) {
+        var delta = 0;
+        if (event.wheelDelta) delta = event.wheelDelta / 120;
+        else if (event.detail) delta = -event.detail / 3;
+
+        handle(delta);
+        if (event.preventDefault) event.preventDefault();
+        event.returnValue = false;
+    }
+
+    function handle(delta) {
+        var time = 350; // delay time
+        var distance = 120; // delta point 
+        // Dom where it will apply 
+        $('html, body').stop().animate({
+            scrollTop: $(window).scrollTop() - (distance * delta)
+        }, {
+            duration: time,
+            easing: 'easeOutCirc'
+        });
+    }
+
+
+
+});
